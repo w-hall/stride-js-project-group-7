cc291e841af220b2244d9a66ef553b97679450da
Finn Zink <finnzink1@gmail.com>
2023-11-10 14:53:37 -0600

    small refactor

--- scripts/impressionismIndex.js
+++ scripts/impressionismIndex.js
@@ -1,13 +1,6 @@
 const observer = new IntersectionObserver ((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
-
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-
+        entry.target.classList.toggle('show', entry.isIntersecting);
     });
 });
 
